25d5fbc19b521a7757b2ef94a2fc47619abb92a9
Torrescayo <126293814+Torrescayo@users.noreply.github.com>
2023-11-06 21:49:22 -0600

    Add files via upload

--- star_06_11_23.py
+++ star_06_11_23.py
@@ -2,7 +2,7 @@ import streamlit as st
 from joblib import load
 import numpy as np
 
-# Load your trained model
+# Loading model
 model = load('/home/torrescayo/ML/finalized_model.sav')
 
 def feature_input():
@@ -17,7 +17,7 @@ def feature_input():
     MJD = st.number_input('MJD')
     return np.array([[u, g, r, i, z, spec_obj_ID, redshift, plate, MJD]]).reshape(1, -1)
 
-# Set up your web app using Streamlit
+# Set up the app
 st.title('Star Classification Predictive Model')
 
 features = feature_input()
@@ -29,10 +29,9 @@ class_map = {
 }
 
 
-# When 'Predict' button is clicked, make a prediction and display it
+# Make a prediction and display it
 if st.button('Predecir'):
     prediction = model.predict(features)
     st.write(f'El objeto astronómico es: {class_map[prediction[0]]}')
 
-# Explanation:
-# Streamlit is used to create a simple web app that can take user input, pass it to the trained model, and display the prediction. The model is loaded using `joblib.load()`.
+
